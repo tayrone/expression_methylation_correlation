83dffe889f4a336306d07ab34a807d2f19b48cbe
Tayrone Monteiro <tayronemonteiro@MacBook-Pro-de-Tayrone.local>
2021-04-06 13:37:47 -0300

    correct implementation

--- correlation.R
+++ correlation.R
@@ -1,29 +1,50 @@
 library(tidyverse)
-library(gdata)
 
+#---- Loads and organizes expression data for further use ----
 
-load("./rdatas_methylation/g3_signature.RData")
+load("./rdatas/shh_network_dictionary.RData")
 
+colnames(gexp) <- str_remove(colnames(gexp), ".CEL")
 
-hits_expression <- pheno_ids %>% inner_join(tibble(gene = hits)) %>% 
-  select(expression = exp_values, everything())
+dictionary <- dictionary %>% mutate(across(probe_id, as.character))
 
-
-gdata::keep(hits_expression, sure = T)
-
-
-load("./rdatas_methylation/group3_probewised.RData")
-
-methylation_coefficient <- as.data.frame(fit[["coefficients"]]) %>% 
+gexp <- gexp %>% 
   rownames_to_column(var = "probe_id") %>% 
-  select(everything(), methylation = `control-Group3`)
-  
+  inner_join(dictionary) %>% 
+  select(probe_id, gene = gene_symbol, everything())
 
-test <- diff_methylated %>% 
+gdata::keep(gexp, sure = T)
+
+
+#---- Organizes methylation data for further use ----
+
+load("./rdatas/shh_filtered.RData")
+load("./rdatas_methylation/shh_probewised.RData")
+
+beta_values <- as.data.frame(getBeta(filtered_quantile_normalized))
+
+diff_methylated <- diff_methylated %>% 
   as_tibble() %>% 
   select(probe_id = name, probe_location = ucsc_refgene_group, 
-         gene = ucsc_refgene_name, aveexpr) %>% 
-  inner_join(methylation_coefficient)
+         gene = ucsc_refgene_name)
 
-hist(test$methylation)
-hist(test$aveexpr)
+colnames(beta_values) <- str_extract(colnames(beta_values), "[^_]+")
+
+beta_values <- beta_values %>% 
+  rownames_to_column(var = "probe_id") %>%
+  inner_join(diff_methylated) %>% 
+  select(probe_id, probe_location, gene, everything()) %>% 
+  drop_na(gene)
+
+gdata::keep(gexp, beta_values, sure = T)
+
+
+#---- 
+# Implementar teste de correlação em si. Talvez seja interessante aumentar o
+# data frame gexp para que ele possa ser pareado com o beta_values, linha a linha.
+
+cor.test(beta_values[, -c("probe_id", "probe_location", "gene")])
+
+corr = cor.test(as.numeric(bVals_Tumor[which(rownames(bVals_Tumor) == bValsPlot$sonda[i]),]), 
+                as.numeric(signature_gexp[which(signature_dictionary$gene_symbol == bValsPlot$gene[i]),]),
+                method = "spearman")
